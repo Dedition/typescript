0d8b266cad72884805bbede625deb0e0015cf7f8
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-10-14 19:09:23 -0400

    Got rid of examples

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,37 +1,6 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let printName: (name: string) => never;
-
-
-// let personName: unknown;
-
-// interface Person {
-//   name: string;
-//   age?: number;
-// }
-
-// interface Guy extends Person {
-//   profession: string;
-// }
-
-// type X = {
-//   a: string;
-//   b: number;
-// }
-
-// let y: Guy = {
-//   name: 'Leo',
-//   profession: 'Software Enginerr'
-// }
-
-
 function App() {
   return (
     <div className="App">
