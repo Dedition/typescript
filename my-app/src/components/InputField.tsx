af78fef7d0974ad60568afcc5de09528f6095869
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-10-14 19:47:32 -0400

    Finished beginning styles for inputfield

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../styles.css";
 
 export const InputField: React.FC<{}> = () => {
     return (
         <form className="input">
             <input className="input__box" type='input' placeholder='Enter a task!' />
-            <button className="input__submit" type="submit">Go</button>
+            <button className="input_submit" type="submit">Go</button>
         </form>
     )
 }
