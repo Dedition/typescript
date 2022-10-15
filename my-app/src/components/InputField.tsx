066c8bbe6f1f140f9dc6099adf9581075141ad4f
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-10-15 16:51:38 -0400

    Created model file with interface

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -7,7 +7,13 @@ interface Props {
 }
 
 export const InputField: React.FC<Props> = ({ todo, setTodo }) => {
-
+    /*
+    The onChange event handler is called whenever the value of the input field changes.
+    The event handler is passed an event object that contains the new value of the input field.
+    The onClose event handler is called when the user clicks the close button.
+    The onKeyPress event handler is called when the user presses a key.
+    
+    */
     return (
         <form className="input">
             <input
